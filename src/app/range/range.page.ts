a8f58d639a21a37197bbaba1025f0e3ddaf26020
xuanhoando <dxh2468@gmail.com>
2022-12-19 01:17:46 +0700

    test
    next commit

--- src/app/range/range.page.ts
+++ src/app/range/range.page.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class RangePage implements OnInit {
 
   constructor() { }
-
+  pinFormatter(value: number) {
+    return `-${value}`;
+  }
   ngOnInit() {
   }
 
