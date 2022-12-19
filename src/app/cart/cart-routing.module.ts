fc19de1975a14f0bd0fb6086845e5bc53892f0c8
xuanhoan99 <dxuanhoan99@gmail.com>
2022-12-19 22:35:42 +0700

    cart

--- src/app/cart/cart-routing.module.ts
+++ src/app/cart/cart-routing.module.ts
@@ -3,11 +3,40 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CartPage } from './cart.page';
 
-const routes: Routes = [
+const routes: Routes = [  
   {
     path: '',
-    component: CartPage
+    component: CartPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+      },
+      {
+        path: 'cart',
+        loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
+      },
+      {
+        path: 'order',
+        loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
+      },
+      {
+        path: 'notify',
+        loadChildren: () => import('./notify/notify.module').then( m => m.NotifyPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/cart/cart',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/cart/cart',
+    pathMatch: 'full'
   }
+
 ];
 
 @NgModule({
